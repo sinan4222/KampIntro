feaf0b18ef06f45ad0fae4d517b0b0ed3a033686
Sinan Bağlantı <baglantisinan1@gmail.com>
2021-07-20 21:43:28 +0300

    4.Gün Sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    //MyList ' imde T ile çalışacağım ne anlama geliyor ? T vermemizin sebebi kullanıcı ister string ister int değer girsin
+    // kısacası saça göre tarak vurmak diyebiliriz .
+    class MyList<T>
+    {
+
+        T[] items ;
+        //ctor  -> constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+
+
+        }
+    }
+}
